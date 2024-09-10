f084ec12209ef434b34d9fe69aa794ad0bcea520
Kovács Zoltán <kovacs.zoltan@softc.hu>
2024-09-10 14:54:12 +0200

    mentés

--- app/Modules/ShinyModule/resources/views/persons/index.blade.php
+++ app/Modules/ShinyModule/resources/views/persons/index.blade.php
@@ -1,5 +1,8 @@
 <x-softc::layouts>
     <x-slot:title>
-        @lang('softc::app.persons.tite')
+        @lang('softc::app.persons.title')
     </x-slot:title>
+
+    <!-- Head Details Section -->
+     {!! !!}
 </x-softc::layouts>
